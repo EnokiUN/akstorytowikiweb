b37a97f743da9a097071ab616d8e954cd0637d33
Enoki <enokiun@gmail.com>
2025-04-21 08:00:22 +0300

    chore: update

--- www/dist/bootstrap.js
+++ www/dist/bootstrap.js
@@ -155,7 +155,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/akstorytowikiweb_bg.wasm":"6c8184db370fc2535774"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/akstorytowikiweb_bg.wasm":"4c10dee087ca4f9aa559"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
