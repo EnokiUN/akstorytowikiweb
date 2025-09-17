61e3660d3470db308373ce3ad6c5c26322089b6d
Enoki <enokiun@gmail.com>
2025-09-17 17:28:43 +0300

    chore: update akstorytowiki

--- www/dist/bootstrap.js
+++ www/dist/bootstrap.js
@@ -155,7 +155,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/akstorytowikiweb_bg.wasm":"8d7339e08c8ef9f8a629"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/akstorytowikiweb_bg.wasm":"bfec06715db98df3c571"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
